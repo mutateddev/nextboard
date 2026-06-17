710272662ddccd7b02a6aa1ec8f412877a10c702
mutateddev <mutateddev@gmail.com>
2026-06-17 22:20:46 +0330

    feat: implement toggle theme with tooltip

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,11 +1,17 @@
+import LightDarkToggle from '@/components/ui/light-dark-toggle';
+
 type Props = {
   children: React.ReactNode;
 };
 
 export default function LoggedOutLayout({ children }: Props) {
   return (
-    <div className='flex flex-col gap-4 min-h-dvh items-center p-24 justify-center'>
-      {children}
-    </div>
+    <>
+      <div className='flex flex-col gap-4 min-h-dvh items-center p-24 justify-center'>
+        {children}
+      </div>
+
+      <LightDarkToggle className='fixed right-0 top-1/2 -translate-y-1/2' />
+    </>
   );
 }
---
+++ components/ui/light-dark-toggle.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { MoonIcon, SunIcon } from 'lucide-react';
+import { Button } from './button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './tooltip';
+
+const LightDarkToggle = ({ className }: { className?: string }) => {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger
+          asChild
+          className={className}
+          onClick={() => {
+            setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
+          }}>
+          <Button variant={'outline'}>
+            <SunIcon className='block dark:hidden' />
+            <MoonIcon className='hidden dark:block' />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <span className='hidden dark:inline'>Enable light Mode</span>
+          <span className='inline dark:hidden'>Enable dark Mode</span>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default LightDarkToggle;
