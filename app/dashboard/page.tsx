bd79c6a57c3305857a750b6a946e37d551836b9f
mutateddev <mutateddev@gmail.com>
2026-06-20 16:46:34 +0330

    feat(dashboard): add employee stats overview

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,17 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import EmployeesStats from './components/employees-stats';
 
 const DashboardPage = () => {
   return (
     <div>
       <Tabs defaultValue='employees'>
-        <TabsList>
+        <TabsList className='mb-4'>
           <TabsTrigger value='employees'>Employees stats</TabsTrigger>
           <TabsTrigger value='teams'>Teams stats</TabsTrigger>
         </TabsList>
-        <TabsContent value='employees'>employees stats view</TabsContent>
+        <TabsContent value='employees'>
+          <EmployeesStats />
+        </TabsContent>
         <TabsContent value='teams'>teams stats view</TabsContent>
       </Tabs>
     </div>
